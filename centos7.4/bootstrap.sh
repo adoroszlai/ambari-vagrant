f7b7471c5b8c896aa65ec529d5d87f4f25ceb36a
Attila Doroszlai <adoroszlai@apache.org>
2019-01-02 12:38:06 +0100

    cleanup centos7.4 bootstrap
     * dir permission 700 instead of 600
     * remove dead code
     * remove unnecessary 'sudo'

--- centos7.4/bootstrap.sh
+++ centos7.4/bootstrap.sh
@@ -2,22 +2,19 @@
 
 cp /vagrant/hosts /etc/hosts
 cp /vagrant/resolv.conf /etc/resolv.conf
-yum install ntp -y
-service ntpd start
-#service iptables stop
-mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
 
-#Again, stopping iptables
-# /etc/init.d/iptables stop
+yum install -y ntp
+service ntpd start
+
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
 
 # Increasing swap space
-sudo dd if=/dev/zero of=/swapfile bs=1024 count=1024k
-sudo mkswap /swapfile
-sudo swapon /swapfile
+dd if=/dev/zero of=/swapfile bs=1024 count=1024k
+mkswap /swapfile
+swapon /swapfile
 echo "/swapfile       none    swap    sw      0       0" >> /etc/fstab
 
-sudo cp /vagrant/insecure_private_key /root/ec2-keypair
-sudo chmod 600 /root/ec2-keypair
-
-# Workaround from https://www.digitalocean.com/community/questions/can-t-install-mysql-on-centos-7
-# rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+cp /vagrant/insecure_private_key /root/ec2-keypair
+chmod 600 /root/ec2-keypair
